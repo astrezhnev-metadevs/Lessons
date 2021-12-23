33047924d8ce6be576eea714f90baa69b46ba601
astrezhnev_metadevs <astrezhnev@meta-devs.io>
2021-12-23 18:30:59 +0500

    1

---
+++ src/com/company/Lesson1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lesson1 {  public static void main(String[] args) {
+//        System.out.println("Hello, world!");
+    int a = 0;
+    int b = 5;
+    int sum = a + b;
+    System.out.println("Сумма " + sum);
+    int multiply = a * b;
+    System.out.println("Произведение " + multiply);
+    int difference = a - b;
+    System.out.println("Разность " + difference);
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.next();
+    String userInput = new Scanner(System.in).next();
+    System.out.println(userInput);
+}
+}
